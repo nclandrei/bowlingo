88e0b91c3fa8d54af00e78c5067f587773c61b12
Andrei-Mihai Nicolae <nclandreimihai@gmail.com>
2018-07-06 16:11:17 +0300

    Fix issue with cross-origin resources access

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"github.com/rs/cors"
+	"net/http"
+)
 
 func main() {
 	server := NewServer()
 	server.routes()
-	http.ListenAndServe(":8000", server.router)
+	http.ListenAndServe(":8000", cors.Default().Handler(server.router))
 }
