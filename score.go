2b261b1725b4e8de5c2792ec3f91259be56e93c1
Andrei-Mihai Nicolae <nclandreimihai@gmail.com>
2018-07-06 03:18:53 +0300

    Update score.go with documentation and better naming for functions

--- score.go
+++ score.go
@@ -29,25 +29,30 @@ func Score(frames []Frame) (*int, error) {
 			score += 10 + spareBonus(frameIndex, rolls)
 			frameIndex += 2
 		} else {
-			score += sumOfBallsInFrame(frameIndex, rolls)
+			score += frameScore(frameIndex, rolls)
 			frameIndex += 2
 		}
 	}
 	return &score, nil
 }
 
+// isStrike checks whether the current frame contains a strike.
 func isStrike(frameIndex int, rolls []int) bool {
 	return rolls[frameIndex] == 10
 }
 
+// isSpare checks whether the current frame contains a spare.
 func isSpare(frameIndex int, rolls []int) bool {
 	return rolls[frameIndex]+rolls[frameIndex+1] == 10
 }
 
-func sumOfBallsInFrame(frameIndex int, rolls []int) int {
+// frameScore returns the sum of the two rolls in the current frame.
+func frameScore(frameIndex int, rolls []int) int {
 	return rolls[frameIndex] + rolls[frameIndex+1]
 }
 
+// spareBonus calculates the score to be added to 10 when the
+// player hits a spare in the current frame.
 func spareBonus(frameIndex int, rolls []int) int {
 	if frameIndex+2 >= len(rolls) {
 		return 0
@@ -55,6 +60,8 @@ func spareBonus(frameIndex int, rolls []int) int {
 	return rolls[frameIndex+2]
 }
 
+// strikeBonus calculates the score to be added to 10 when the
+// player hits a strike in the current frame.
 func strikeBonus(frameIndex int, rolls []int) int {
 	if frameIndex+2 >= len(rolls) {
 		return 0
@@ -62,6 +69,9 @@ func strikeBonus(frameIndex int, rolls []int) int {
 	return rolls[frameIndex+1] + rolls[frameIndex+2]
 }
 
+// framesToRolls converts a slice of frames to a slice of rolls. This is needed
+// because, if we only used frames, there would have been code smells, such as
+// nested if/else if/else blocks. This way, the code is much cleaner.
 func framesToRolls(frames []Frame) []int {
 	var rolls []int
 	for _, f := range frames {
