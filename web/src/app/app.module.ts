f4592fa486c007b94db24faa4c3c7e066cbe7335
Andrei-Mihai Nicolae <nclandreimihai@gmail.com>
2018-07-06 16:28:18 +0300

    Add Angular Bootstrap to the frontend

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { ScoreService } from './services/score.service';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [ScoreService],
   bootstrap: [AppComponent]
