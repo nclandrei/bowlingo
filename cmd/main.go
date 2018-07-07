5edb3bb1476565b2e26f3b735aa3d76f52980e26
Andrei-Mihai Nicolae <nclandreimihai@gmail.com>
2018-07-07 03:24:50 +0300

    Add flags for the main command

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/nclandrei/bowlingo"
 	"github.com/rs/cors"
 	"net/http"
 )
 
+var (
+	address = flag.String("addr", "localhost", "hostname for the bowling server")
+	port    = flag.Int("port", 8000, "port for the bowling server")
+)
+
 func main() {
+	flag.Parse()
 	server := bowlingo.NewServer()
 	server.Routes()
-	http.ListenAndServe(":8000", cors.Default().Handler(server))
+	http.ListenAndServe(fmt.Sprintf("%s:%d", *address, *port), cors.Default().Handler(server))
 }
