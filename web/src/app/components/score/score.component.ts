ec80e93fbfaedba840a60a909c745683fad73450
Andrei-Mihai Nicolae <nclandreimihai@gmail.com>
2018-07-07 06:29:56 +0300

    Finish score component with proper displaying

--- web/src/app/components/score/score.component.ts
+++ web/src/app/components/score/score.component.ts
@@ -13,7 +13,7 @@ export class ScoreComponent implements OnInit {
   private totalScore = 0;
   private firstRoll: number;
   private secondRoll: number;
-  private bonusRoll = null;
+  private bonusRoll: number;
 
   constructor(private scoreService: ScoreService) {}
 
@@ -23,7 +23,7 @@ export class ScoreComponent implements OnInit {
     const currentFrame: Frame = {
       firstRoll: this.firstRoll,
       secondRoll: this.secondRoll,
-      bonusRoll: this.bonusRoll,
+      bonusRoll: (this.frames.length === 9) ? this.bonusRoll : null,
     };
     this.frames.push(currentFrame);
     this.scoreService.sendFrames(this.frames)
@@ -35,5 +35,39 @@ export class ScoreComponent implements OnInit {
   private resetGame() {
     this.frames = [];
     this.totalScore = 0;
+  }
+
+  private getRollText(rollIndex: number, frame: Frame) {
+    console.log(frame);
+    switch (rollIndex) {
+      case 0:
+        if (frame.firstRoll === 10) {
+          return 'X';
+        } else {
+          return frame.firstRoll.toString();
+        }
+      case 1:
+        if (frame.firstRoll === 10) {
+          return '\u00a0';
+        } else if (frame.firstRoll + frame.secondRoll === 10) {
+          return '/';
+        } else {
+          return frame.secondRoll.toString();
+        }
+      case 2:
+        if (this.frames.length < 10) {
+          break;
+        }
+        if (frame.bonusRoll === 10) {
+          return 'X';
+        } else {
+          return frame.bonusRoll;
+        }
+    }
+  }
+
+  private isBonusRollAwarded(): boolean {
+    return this.frames.length === 9 &&
+      (this.frames[8].firstRoll === 10 || this.frames[8].firstRoll + this.frames[8].secondRoll === 10);
   }
 }
