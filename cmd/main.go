c100761d118498be540557dccad5821f61648dcc
Andrei-Mihai Nicolae <nclandreimihai@gmail.com>
2018-07-07 03:29:26 +0300

    Add comment explaining folder layout for the application

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+/*
+	The command for launching the server is stored in the cmd folder as it is considered
+	best practice in the Go community - it differentiates the command from the library which
+	can be used as a standalone product as well. For more details, please see here:
+	https://medium.com/@benbjohnson/standard-package-layout-7cdbc8391fc1 and
+	https://medium.com/@benbjohnson/structuring-applications-in-go-3b04be4ff091
+*/
+
 import (
 	"flag"
 	"fmt"
